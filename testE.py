07f498c49755f417d1de1a13fd6233dce1239ac8
Tangui Nzogne <32547259+tangui44@users.noreply.github.com>
2021-03-02 01:24:53 +0100

    Update testE.py

--- testE.py
+++ testE.py
@@ -1,65 +1,42 @@
-import csv
-import sys
-
-pathE= sys.argv[1]
-pathS = sys.argv[2]
-
-ligne = open(pathE,"r") # Lecture du fichier d'entrée 
-r = ligne.readlines()
-print(len(r))
+import fileinput
 
 listzoom = []
 occu = 1
 tampon = ''
+text = ""
 
-myfile = open(pathS, 'w') # Création du fichier de sortie 
-mywriter = csv.writer(myfile, delimiter='\t', dialect='excel', lineterminator='\n')
+for line in fileinput.input():
+    tp = line.split(",")
+    #print(line.rstrip())
+    #print(tp)
 
-for row in r:
-    tp = row.split(",")
     tp1 = tp[1].split("\t")
 
     if(tp1[0].isdigit() and len(tp1[0])==3):
         tp2 = tp1[1].split("/")
 
         # Vérifier si la url suit la forme réquis : /map/1.0/slab/photo/256/16/32798/22739
-        if(tp2[1]== 'map' and tp2[2]== '1.0' and tp2[3]== 'slab' and tp2[5]== '256' and tp2[6].isdigit() and tp2[7].isdigit() and '\n' in tp2[8]):
-            tep = tp2[8].split('\n')
+        if(tp2[1]== 'map' and tp2[2]== '1.0' and tp2[3]== 'slab' and tp2[5].isdigit() and tp2[6].isdigit() and tp2[7].isdigit()):
+            # Vérifie si le viewmode a changé 
+            if(tp2[4]==tampon):
+                occu += 1
 
-            # Vérifie si la dernière partie de l'url est un entier 
-            if(tep[0].isdigit()):
-                # Vérifie si le viewmode a changé 
-                if(tp2[4]==tampon):
-                    occu += 1
-
-                    # Vérifie si le niveau de Zoom est déjà dans la liste des zoom
-                    if(not(tp2[6]in listzoom)):
-                        listzoom.append(tp2[6])
-                    # Vérifie si c'est la dernière ligne 
-                    if(row == r[len(r)-1]) :
-                        # on écrit sur le fichier de sortie 
-                        mywriter.writerow([tampon,str(occu),' '.join(listzoom) ])
+                # Vérifie si le niveau de Zoom est déjà dans la liste des zoom
+                if(not(tp2[6]in listzoom)):
+                    listzoom.append(tp2[6])
+            else :
+                # Vérifie si c'est la première ligne 
+                if(fileinput.isfirstline()):
+                    occu = 1 
+                    tampon = tp2[4]
+                    listzoom = []
+                    listzoom.append(tp2[6])
                 else :
-                    # Vérifie si c'est la première ligne 
-                    if(row == r[0]):
-                        occu = 1 
-                        tampon = tp2[4]
-                        listzoom = []
-                        listzoom.append(tp2[6])
-                    # Vérifie si c'est la dernière ligne 
-                    elif(row == r[len(r)-1]) :
-                        mywriter.writerow([tampon,str(occu),' '.join(listzoom) ])
-                        occu = 1 
-                        tampon = tp2[4]
-                        listzoom = []
-                        listzoom.append(tp2[6])
-                        mywriter.writerow([tampon,str(occu),' '.join(listzoom) ])
-                    else :
-                        mywriter.writerow([tampon,str(occu),' '.join(listzoom) ])
-                        occu = 1 
-                        tampon = tp2[4]
-                        listzoom = []
-                        listzoom.append(tp2[6])
-                    
-
-
+                    text = tampon +'\t'+ str(occu) + '\t' + ','.join(listzoom) + '\n'
+                    sys.stdout.write(text)
+                    occu = 1 
+                    tampon = tp2[4]
+                    listzoom = []
+                    listzoom.append(tp2[6])
+text = tampon +'\t'+ str(occu) + '\t' + ','.join(listzoom) + '\n'
+sys.stdout.write(text)
